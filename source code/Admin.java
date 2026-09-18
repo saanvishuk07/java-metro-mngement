1d13b814918ac9edd0e2f35768939c74acaec7d0
saanvishuk07 <saanvi.25bai10111@vitbhopal.ac.in>
2026-09-18 22:29:48 +0530

    Add files via upload

---
+++ source code/Admin.java
@@ -0,0 +1,12 @@
+package metro;
+
+public class Admin extends User {
+    public Admin(int id, String name, String phone) {
+        super(id, name, phone);
+    }
+
+    @Override
+    public String getRole() {
+        return "ADMIN";
+    }
+}
